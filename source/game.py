6f844a9a929e436658e7afbb32c3c5432b36ad37
Rúben Tadeia <125905559+rubenftgd@users.noreply.github.com>
2023-06-23 19:59:17 +0100

    Added 2 classes

--- source/game.py
+++ source/game.py
@@ -1,7 +1,30 @@
+import pygame
 import window as win
 
-if __name__ == "__main__":
-	# Create a window
-	window = win.Window()
+class Game:
+	def __init__(self):
+		pygame.init()
+		self.window = None
 
-	window.createGame()
+	def run(self):
+		self.window = win.Window()
+
+		# Game loop
+		running = True
+		while running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					running = False
+
+			# Fill the screen with a solid color
+			self.window.screen.fill((0, 0, 0))
+
+			# Update the full display Surface to the screen
+			pygame.display.flip()
+
+		# Once we have exited the main program loop we can stop the game engine:
+		pygame.quit()
+
+# Run the game
+game = Game()
+game.run()
--- source/window.py
+++ source/window.py
@@ -1,31 +1,11 @@
+# window.py
 import pygame
 
 class Window:
-	# The constructor
-	def __init__(self, width = 800,height=600):
-		# Initialize the Pygame
-		pygame.init()
-		# Set the dimensions of the window
+	def __init__(self, width=800, height=600):
 		self.width = width
 		self.height = height
 		self.screen = pygame.display.set_mode((width, height))
-
-		# Fill the screen with a solid color
-		self.screen.fill((0, 0, 0))
-
-	def createGame(self):
-		# Game loop
-		running = True
-		while running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					running = False
-
-			# Fill the screen with a solid color
-			self.screen.fill((0, 0, 0))
-
-			# Update the full display Surface to the screen
-			pygame.display.flip()
-
-		# Once we have exited the main program loop we can stop the game engine:
-		pygame.quit()
+		pygame.display.set_caption("Pygame Window")
+		self.screen.fill((0, 0, 0))  # Fill the screen with a solid color
+		pygame.display.flip()  # Update the display after filling it
