703acdaaf3655d24894c55a853844450a79f56f9
Marcelo <mrgo2000@gmail.com>
2023-06-24 13:19:20 +0100

    read movement key events

--- source/game.py
+++ source/game.py
@@ -2,29 +2,77 @@ import pygame
 import window as win
 
 class Game:
-	def __init__(self):
-		pygame.init()
-		self.window = None
+    def __init__(self, screen_width, screen_height, fps):
+        # Initialize Pygame
+        pygame.init()
 
-	def run(self):
-		self.window = win.Window()
+        # Set up the game window
+        self.screen = pygame.display.set_mode((screen_width, screen_height))
+        pygame.display.set_caption("My Game")
 
-		# Game loop
-		running = True
-		while running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					running = False
+        # Set up the game clock
+        self.clock = pygame.time.Clock()
+        self.fps = fps
 
-			# Fill the screen with a solid color
-			self.window.screen.fill((0, 0, 0))
+        # Game state
+        self.running = False
+        self.paused = False
 
-			# Update the full display Surface to the screen
-			pygame.display.flip()
+    def start(self):
+        self.running = True
+        self.game_loop()
 
-		# Once we have exited the main program loop we can stop the game engine:
-		pygame.quit()
+    def pause(self):
+        self.paused = not self.paused
 
-# Run the game
-game = Game()
-game.run()
+    def restart(self):
+        self.running = False
+        self.paused = False
+        self.start()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    self.pause()
+                elif event.key == pygame.K_r:
+                    self.restart()
+                elif event.key == pygame.K_w:
+                    print("Move the character forwards")
+                elif event.key == pygame.K_s:
+                    print("Move the character backwards")
+                elif event.key == pygame.K_a:
+                    print("Move the character left")
+                elif event.key == pygame.K_d:
+                    print("Move the character right")
+
+
+    def update_game_logic(self):
+        if not self.paused:
+            # Update game state
+            pass
+
+    def render(self):
+        # Render the game elements
+        self.screen.fill((0, 0, 0))  # Example background fill
+
+        # Add your rendering code here
+
+        # Update the screen
+        pygame.display.flip()
+
+    def game_loop(self):
+        while self.running:
+            self.handle_events()
+            self.update_game_logic()
+            self.render()
+            self.clock.tick(self.fps)
+
+        pygame.quit()
+
+if __name__ == "__main__":
+	# Create a game instance and start it
+	game = Game(800, 600, 60)
+	game.start()
